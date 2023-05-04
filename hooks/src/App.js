d15c4973f6d13b6becff2ef6bb84f893bcca6825
Christian gabe <cgngoran@gmail.com>
2023-05-04 03:48:48 +0000

    first commit

---
+++ hooks/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MovieList from './Components/MovieList';
+import './Components/style.css';
+import './Components/MovieCard';
+
+
+
+
+function App () {
+
+return(
+<>
+<MovieList/>
+</>
+
+
+)
+
+
+}
+
+export default App;
